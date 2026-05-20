c02b3d9c4ea0bd2f96ff3ed467a801fe9b864da2
jorgemagic <jferrero@plainconcepts.com>
2026-05-20 17:43:09 +0200

    - Fixed loading indicator

--- IARendering.Avalonia/Controls/LoadingSpinner.axaml.cs
+++ IARendering.Avalonia/Controls/LoadingSpinner.axaml.cs
@@ -1,6 +1,5 @@
 using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
-using Avalonia.Media;
 using Avalonia.Threading;
 using System;
 
@@ -8,8 +7,11 @@ namespace IARendering.Avalonia.Controls
 {
     public partial class LoadingSpinner : UserControl
     {
+        private static readonly double[] BaseOpacities = [1.00, 0.88, 0.76, 0.64, 0.52, 0.40, 0.28, 0.16];
+
         private readonly DispatcherTimer timer;
-        private Path? spinnerArc;
+        private Ellipse[] dots = [];
+        private int frameOffset;
 
         public LoadingSpinner()
         {
@@ -17,14 +19,23 @@ namespace IARendering.Avalonia.Controls
 
             this.timer = new DispatcherTimer
             {
-                Interval = TimeSpan.FromMilliseconds(16),
+                Interval = TimeSpan.FromMilliseconds(80),
             };
 
             this.timer.Tick += this.TimerTick;
             this.AttachedToVisualTree += this.OnAttachedToVisualTree;
             this.DetachedFromVisualTree += this.OnDetachedFromVisualTree;
-
-            this.spinnerArc = this.FindControl<Path>("SpinnerArc");
+            this.dots =
+            [
+                this.FindControl<Ellipse>("Dot0"),
+                this.FindControl<Ellipse>("Dot1"),
+                this.FindControl<Ellipse>("Dot2"),
+                this.FindControl<Ellipse>("Dot3"),
+                this.FindControl<Ellipse>("Dot4"),
+                this.FindControl<Ellipse>("Dot5"),
+                this.FindControl<Ellipse>("Dot6"),
+                this.FindControl<Ellipse>("Dot7"),
+            ];
         }
 
         private void OnAttachedToVisualTree(object? sender, global::Avalonia.VisualTreeAttachmentEventArgs e)
@@ -39,9 +50,17 @@ namespace IARendering.Avalonia.Controls
 
         private void TimerTick(object? sender, EventArgs e)
         {
-            if (this.spinnerArc?.RenderTransform is RotateTransform rotateTransform)
+            if (this.dots.Length != BaseOpacities.Length)
             {
-                rotateTransform.Angle = (rotateTransform.Angle + 6) % 360;
+                return;
+            }
+
+            this.frameOffset = (this.frameOffset + 1) % this.dots.Length;
+
+            for (int index = 0; index < this.dots.Length; index++)
+            {
+                int opacityIndex = (index + this.frameOffset) % BaseOpacities.Length;
+                this.dots[index].Opacity = BaseOpacities[opacityIndex];
             }
         }
     }
